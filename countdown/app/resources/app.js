2b0c7a9fc8c150b88f638b2b4bae4de6050ebe11
Tina Colvin <tmorozco1990@gmail.com>
2018-05-07 15:05:47 -0700

    eventTimer object created and logging timestamps in console

--- countdown/app/resources/app.js
+++ countdown/app/resources/app.js
@@ -1,77 +1,27 @@
 // const eventDate = new Date('May 7, 2018 14:00:00').getTime();  // date in the future to count to 
 // const MovingDate = new Date('March 27, 2018 18:00:00').getTime();  // date in the future to count to 
 
-const eventTimer = () => {
-    setEventDate: '';
-    _eventDate: new Date('May 7, 2018 14:00:00').getTime();
-    _now: new Date().getTime();
-    _distance: eventDate - now;
-    _day: Math.floor(distance / (1000 * 60 * 60 * 24));
-    _hour: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    _min: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    _sec: Math.floor((distance % (1000 * 60)) / 1000);
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let x = setInterval( function(){
-
-//     const now = new Date().getTime(); // getting time now
-
-//     distance = eventDate - now;
+const eventTimer = class {
+    constructor( eventDate ) { 
+        this.eventDate = new Date(eventDate).getTime();
+        this.now = () => new Date().getTime();
+        this.getDistance = () => this.eventDate - this.now();
+        this._day = () => Math.floor(this.getDistance() / (1000 * 60 * 60 * 24));
+        this._hour = () => Math.floor((this.getDistance() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        this._min = () => Math.floor((this.getDistance() % (1000 * 60 * 60)) / (1000 * 60));
+        this._sec = () => Math.floor((this.getDistance() % (1000 * 60)) / 1000);
+        this.checkTime = () => console.log(this._day(), ":", this._hour(), ":", this._min(), ":", this._sec())
+    }
     
-//     // Time calculations for days, hours, minutes and seconds
-//     var days = 
-//     var hours = 
-//     var minutes = 
-//     var seconds = 
+};
+ 
+const month = 'May'
+const day = '7'
+const hr = '15'
+const min = '30'
+const sec = '00'
 
-//     // for two zeros instead of one and also proper formatting of the card 
-//     if (days < 10) days = '0' + days;
-//     if (hours < 10) hours = '0' + hours;
-//     if (minutes < 10) minutes = '0' + minutes;
-//     if (seconds < 10) seconds = '0' + seconds;
+const pomorado = new eventTimer(`${month} ${day}, 2018 ${hr}:${min}:${sec}`);
+console.log(pomorado);
 
-//     // Display the result in the element with id="demo"
-//     document.getElementById("day").innerHTML = days;
-//     document.getElementById("hour").innerHTML = hours;
-//     document.getElementById("min").innerHTML = minutes;
-//     document.getElementById("sec").innerHTML = seconds;
-
-//     // mute items that have already counted all the way down
-//     if ($('#day').html() === '00') $('#d').addClass('text-muted') && $('#day').addClass('text-muted');
-//     if ($('#hour').html() === '00') $('#h').addClass('text-muted') && $('#hour').addClass('text-muted');
-//     if ($('#min').html() === '00')  $('#m').addClass('text-muted') && $('#min').addClass('text-muted');
-//     if ($('#sec').html() === '00' & $('#min').html() === '00')  $('#s').addClass('text-muted') && $('#sec').addClass('text-muted');
-    
-//     // if ($('#sec').html() !== '00' && $('#sec').hasClass('text-muted')) $('#sec').removeClass('text-muted')
-//     if ($('#hour').html() !== '00' && $('#hour').hasClass('text-muted')) $('#hour').removeClass('text-muted') && $('#h').removeClass('text-muted');
-//     if ($('#min').html() !== '00' && $('#min').hasClass('text-muted')) $('#min').removeClass('text-muted')&& $("#m").removeClass('text-muted');
-//     if ($('#sec').html() !== '00' && $('#sec').hasClass('text-muted')) $('#sec').removeClass('text-muted')&& $("#s").removeClass('text-muted');
-    
-    
-//     // If the count down is finished, clear the interval
-//     if (distance < 0) {
-//         clearInterval(x);
-//     }
-
-// } , 1000);  // 1 sec interval 
-
-
-// $('#menu').click( function (e) {
-//     e.preventDefault();
-//     $('#options').addClass('d-block');
-// });
-
-// // console.log(MovingDate);
+setInterval(pomorado.checkTime, 1000);
